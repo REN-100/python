34c3671e840d356e90ec86bd91db65a6e7adff2d
REN-100 <127933531+REN-100@users.noreply.github.com>
2024-06-28 03:55:18 -0700

    Update Dictionary_keys.py
    a program that checks if a value (entered by the user) is in a dictionary or not.

--- Dictionary_keys.py
+++ Dictionary_keys.py
@@ -4,22 +4,19 @@ my_dict = {"a": "juice", "b": "grill", "c": "corn"}
 data = input()
 
 # create a flag variable and set it to False
-flag = 0
+flag = False
 
 # loop through my_dict
-for data in my_dict:
+for key in my_dict:
     # check if the value entered by the user is in the dictionary or not
-    data in my_dict
+    if data == my_dict[key]:
+        flag = True
+        break
     # if yes, set flag to True and terminate the loop
-    if True:
-        flag = 1
-        break
-    else:
-        flag = 0
-        break
+    
     
 # print value found not found based on the flag status
-if flag == 1:
+if flag == True:
     print("Value found")
 else:
     print("Value not found")
